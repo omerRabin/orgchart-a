9bd4f53dd76a6c7b4c2ef3a76d50125fdf5d8ed2
omerRabin <73130830+omerRabin@users.noreply.github.com>
2022-05-18 19:40:53 +0300

    Create Test.cpp

---
+++ Test.cpp
@@ -0,0 +1,14 @@
+#include "doctest.h"
+#include <string>
+#include <stdexcept>
+#include <iostream>
+#include "OrgChart.hpp"
+using namespace std;
+using namespace ariel;
+
+TEST_CASE("")
+{
+	for(int i=0 ;i < 20; i++){
+		CHECK(1==1);
+	}
+}
